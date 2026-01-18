b42b4bd47d908539de70dfd1e65615caa68c13bd
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2026-01-18 19:49:20 +0100

    created YFClient class and the get_ohlcv function which will grab the data, format and return a dataframe

---
+++ src/trilobite/marketdata/client.py
@@ -0,0 +1,42 @@
+from datetime import date
+import yfinance as yf
+from pandas import DataFrame
+
+class YFClient:
+    """
+    Gets the data
+    """
+    def get_ohlcv(self, ticker: str, start_date: date  = date(1900, 1, 1)) -> DataFrame:
+        """
+        Gets the data from the date spesified until today, if no date object 
+        is sent, it defaults to 1900, which is most likely max
+        """
+        t = yf.Ticker(ticker)
+        df = t.history(
+            start=start_date,
+            end=None,
+            interval="1d",
+            actions=True,
+            auto_adjust=False,
+        )
+        #turn date into it's own column, and rename columns to lowercase
+        df = (
+            df
+            .reset_index()
+            .rename(columns={
+                "Date": "date",
+                "Open": "open",
+                "High": "high",
+                "Low": "low",
+                "Close": "close",
+                "Adj Close": "adjclose",
+                "Volume": "volume",
+                "Dividends": "dividends",
+                "Stock Splits": "stocksplits",
+                })
+        )
+        #strip the time and leave the date
+        df["date"] = df["date"].dt.date
+        return df
+        
+    
