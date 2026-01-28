41df908fd013f9e57fad762f1f042ebdd2fb7be7
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2026-01-28 16:48:42 +0100

    moved cliflags to runtimeflags

--- src/trilobite/cli/runtimeflags.py
+++ src/trilobite/cli/runtimeflags.py
@@ -3,8 +3,19 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class RuntimeFlags:
+    #Dev related
     dev: bool = False
     debug: bool = False
     dry_run: bool = False
     consolelog: bool = False
+    #Commands
+    updateall: bool = False
+    train_nn: bool = False
+    #Settings
+    topn: int | None = None # 20
+    n_factors: int | None = None # 20
+    min_days: int | None = None # 1260
+    lookback: int | None = None # 60
+    horizon: int | None = None # 1
+    epochs: int | None = None # 10
 
