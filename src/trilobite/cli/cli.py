fa67f64e02864e7f0467ea1598c2a2c7cd7511c5
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2026-02-06 10:42:17 +0100

    moved models to config.py, removed min_days in config, removed load.py, models.py, defaults.py, cli.py now creates the appconfig dataclass and returns it along with cliflags

--- src/trilobite/cli/cli.py
+++ src/trilobite/cli/cli.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import argparse
 from trilobite.cli.runtimeflags import CliFlags, ConfigFlags
+from trilobite.config.models import AppConfig, CFGAnalysis, CFGDataBase, CFGDev, CFGMisc, CFGTickerService
 
 def parse_args(argv: list[str]) -> tuple[ConfigFlags, CliFlags]:
     """
@@ -17,7 +18,7 @@ def parse_args(argv: list[str]) -> tuple[ConfigFlags, CliFlags]:
     p.add_argument("--default-timedelta", type=int, help="Default how many days of overlap when updating")
     p.add_argument("--topn", type=int, help="Top N tickers to display")
     p.add_argument("--n-factors", type=int, help="Components for PCA extraction")
-    p.add_argument("--min-days", type=int, help="Minimum trading days required in DB")
+    #p.add_argument("--min-days", type=int, help="Minimum trading days required in DB")
     p.add_argument("--lookback", type=int, help="Lookback window (days)")
     p.add_argument("--horizon", type=int, help="Prediction horizon (days)")
     p.add_argument("--epochs", type=int, help="Training epochs")
@@ -30,28 +31,41 @@ def parse_args(argv: list[str]) -> tuple[ConfigFlags, CliFlags]:
 
 
     ns = p.parse_args(argv)
-
-    configflags = ConfigFlags(
+    #config
+    dev = CFGDev(
         dev = ns.dev,
         debug=ns.debug or ns.dev,
         dry_run=ns.dry_run,
         consolelog=ns.consolelog,
+    )
+    tickerservice = CFGTickerService(
         default_date=ns.default_date,
         default_timedelta=ns.default_timedelta,
-        topn=ns.topn,
+    )
+    db = CFGDataBase()
+    misc = CFGMisc()
+    analysis = CFGAnalysis(
+        top_n=ns.topn,
         n_factors=ns.n_factors,
-        min_days=ns.min_days,
         lookback=ns.lookback,
         horizon=ns.horizon,
         epochs=ns.epochs,
         period=ns.period,
         ticker=ns.ticker,
     )
+    cfg = AppConfig(
+        dev=dev,
+        ticker=tickerservice,
+        db=db,
+        misc=misc,
+        analysis=analysis,
+    )
+    #Cliflags
     cliflags = CliFlags(
         updateall=ns.updateall,
         train_nn=ns.train_nn,
         display_graph=ns.display_graph
     )
-    return configflags, cliflags
+    return cfg, cliflags
 
 
