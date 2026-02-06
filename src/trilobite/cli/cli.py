08564ab2c8950da6f01af2cdff2e64873daa4ba3
Kjetil Paulsen <kjetil_paulsen@hotmail.com>
2026-02-06 14:49:06 +0100

    moved some queries over to its own file, also fixed parse argv

--- src/trilobite/cli/cli.py
+++ src/trilobite/cli/cli.py
@@ -16,7 +16,7 @@ def parse_args(argv: list[str]) -> tuple[AppConfig, CliFlags]:
     p.add_argument("--consolelog", action="store_true", help="Logging will output to the console")
     p.add_argument("--default-date", type=str, help="Default date, format 'YYYY-MM-DD'")
     p.add_argument("--default-timedelta", type=int, help="Default how many days of overlap when updating")
-    p.add_argument("--topn", type=int, help="Top N tickers to display")
+    p.add_argument("--top-n", type=int, help="Top N tickers to display")
     p.add_argument("--n-factors", type=int, help="Components for PCA extraction")
     #p.add_argument("--min-days", type=int, help="Minimum trading days required in DB")
     p.add_argument("--lookback", type=int, help="Lookback window (days)")
@@ -32,26 +32,32 @@ def parse_args(argv: list[str]) -> tuple[AppConfig, CliFlags]:
 
     ns = p.parse_args(argv)
     #config
+    def _use_cli_or_cfg(cli_arg, cfg_arg):
+        """
+        """
+        return cli_arg if cli_arg is not None else cfg_arg
+
     dev = CFGDev(
-        dev = ns.dev,
-        debug=ns.debug or ns.dev,
-        dry_run=ns.dry_run,
-        consolelog=ns.consolelog,
+        dev = _use_cli_or_cfg(ns.dev, CFGDev.dev),
+        debug = _use_cli_or_cfg(ns.debug, CFGDev.debug) or _use_cli_or_cfg(ns.dev, CFGDev.dev),
+        dry_run = _use_cli_or_cfg(ns.dry_run, CFGDev.dry_run),
+        consolelog = _use_cli_or_cfg(ns.consolelog, CFGDev.consolelog),
     )
     tickerservice = CFGTickerService(
-        default_date=ns.default_date,
-        default_timedelta=ns.default_timedelta,
+        default_date = _use_cli_or_cfg(ns.default_date, CFGTickerService.default_date),
+        default_timedelta = _use_cli_or_cfg(ns.default_timedelta, CFGTickerService.default_timedelta),
     )
+    #Might need to create flags for these two later
     db = CFGDataBase()
     misc = CFGMisc()
     analysis = CFGAnalysis(
-        top_n=ns.topn,
-        n_factors=ns.n_factors,
-        lookback=ns.lookback,
-        horizon=ns.horizon,
-        epochs=ns.epochs,
-        period=ns.period,
-        ticker=ns.ticker,
+        top_n = _use_cli_or_cfg(ns.top_n, CFGAnalysis.top_n),
+        n_factors = _use_cli_or_cfg(ns.n_factors, CFGAnalysis.n_factors),
+        lookback = _use_cli_or_cfg(ns.lookback, CFGAnalysis.lookback),
+        horizon = _use_cli_or_cfg(ns.horizon, CFGAnalysis.horizon),
+        epochs = _use_cli_or_cfg(ns.epochs, CFGAnalysis.epochs),
+        period = _use_cli_or_cfg(ns.period, CFGAnalysis.period),
+        ticker = _use_cli_or_cfg(ns.ticker, CFGAnalysis.ticker),
     )
     cfg = AppConfig(
         dev=dev,
